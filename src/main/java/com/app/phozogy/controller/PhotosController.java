5532b3e66bc1a35bf944234c9162807124b82cd6
Ankit Bansal(ankit.bansal17) <ankit.bansal17@infosys.com>
2020-09-09 19:02:29 +0530

    get endpoint exposed

--- src/main/java/com/app/phozogy/controller/PhotosController.java
+++ src/main/java/com/app/phozogy/controller/PhotosController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +26,17 @@ public class PhotosController {
 	@PutMapping(value = "/uploadPhotos")
 	public ResponseEntity<List<Resource>> uploadFile(@RequestParam("photo") MultipartFile[] files) {
 		List<Images> imagesList = photoService.uploadPhotos(Arrays.asList(files));
+		List<Resource> byteArrayResourceList = new ArrayList<>();
+		imagesList.stream().forEach(entry->{
+			byteArrayResourceList.add(new ByteArrayResource(entry.getImage()));
+		});
+		return ResponseEntity.ok()
+				.body(byteArrayResourceList);
+	}
+	
+	@GetMapping(value = "/getPhotos")
+	public ResponseEntity<List<Resource>> getFiles(@RequestParam("category") String category) {
+		List<Images> imagesList = photoService.getPhotos(category);
 		List<Resource> byteArrayResourceList = new ArrayList<>();
 		imagesList.stream().forEach(entry->{
 			byteArrayResourceList.add(new ByteArrayResource(entry.getImage()));
