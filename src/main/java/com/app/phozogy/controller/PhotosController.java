32c5ce780dd2e789a6b5bc84f28ffa604d7dad65
Ankit Bansal(ankit.bansal17) <ankit.bansal17@infosys.com>
2020-09-16 18:03:07 +0530

    fetching multiple photos fix

--- src/main/java/com/app/phozogy/controller/PhotosController.java
+++ src/main/java/com/app/phozogy/controller/PhotosController.java
@@ -1,13 +1,9 @@
 package com.app.phozogy.controller;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.app.phozogy.entity.Images;
+import com.app.phozogy.repository.ImageRepository;
 import com.app.phozogy.service.PhotoService;
 
 @RestController
@@ -22,26 +19,17 @@ public class PhotosController {
 
 	@Autowired
 	PhotoService photoService;
+	
+	@Autowired
+	ImageRepository imageRepo;
 
 	@PutMapping(value = "/uploadPhotos")
-	public ResponseEntity<List<Resource>> uploadFile(@RequestParam("photo") MultipartFile[] files) {
-		List<Images> imagesList = photoService.uploadPhotos(Arrays.asList(files));
-		List<Resource> byteArrayResourceList = new ArrayList<>();
-		imagesList.stream().forEach(entry->{
-			byteArrayResourceList.add(new ByteArrayResource(entry.getImage()));
-		});
-		return ResponseEntity.ok()
-				.body(byteArrayResourceList);
+	public List<Images> uploadFile(@RequestParam("photo") MultipartFile[] files) {
+		return photoService.uploadPhotos(Arrays.asList(files));
 	}
 	
 	@GetMapping(value = "/getPhotos")
-	public ResponseEntity<List<Resource>> getFiles(@RequestParam("category") String category) {
-		List<Images> imagesList = photoService.getPhotos(category);
-		List<Resource> byteArrayResourceList = new ArrayList<>();
-		imagesList.stream().forEach(entry->{
-			byteArrayResourceList.add(new ByteArrayResource(entry.getImage()));
-		});
-		return ResponseEntity.ok()
-				.body(byteArrayResourceList);
+	public List<Images> getFiles(@RequestParam("category") String category) {
+		return photoService.getPhotos(category);
 	}
 }
